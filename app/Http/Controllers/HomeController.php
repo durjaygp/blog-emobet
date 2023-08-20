aad474fa026715c1537867b33ad6290ebde60fed
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2023-08-20 16:31:46 +0600

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,9 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('frontEnd.home.home');
+        return view('homePage.master');
     }
+
     public function blogs(){
         $blogs = Post::with('image')->latest()->withCount('comments')->paginate(10);
 
