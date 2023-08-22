3ea18641d2cc302524a3f5bd4e36ad5f2825fd61
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2023-08-22 08:28:06 +0600

    kk

--- resources/views/admin/user/user.blade.php
+++ resources/views/admin/user/user.blade.php
@@ -25,7 +25,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex flex-column align-items-center text-center">
-                                    <img src="{{asset($user->image)}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+                                    <img src="{{asset(optional($user)->image)}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
                                     <div class="mt-3">
                                         <h4>{{$user->name}}</h4>
                                         <p class="text-secondary mb-1">{{$user->description}}</p>
